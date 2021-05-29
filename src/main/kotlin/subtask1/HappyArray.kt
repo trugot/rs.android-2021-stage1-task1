2e5072302365d18784d08f8a77c389e3f35a8295
trugot <77696733+trugot@users.noreply.github.com>
2021-05-29 17:37:31 +0300

    fix: update HappyArray.kt

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -3,7 +3,29 @@ package subtask1
 class HappyArray {
 
     // TODO: Complete the following function
-    fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+   fun convertToHappy(sadArray: IntArray): IntArray {
+    var happyArray: IntArray = sadArray
+    var finalized = false
+    while (!finalized) {
+        finalized = true
+        for ((index, value) in happyArray.withIndex()) {
+            if (index == 0 || index == happyArray.size - 1)
+                continue
+            if (value > (happyArray[index - 1] + happyArray[index + 1])){
+                happyArray = remove(happyArray, index)
+                finalized = false
+                break
+            }
+        }
+    }
+    return happyArray
+    }
+    private fun remove(arr: IntArray, index: Int): IntArray {
+        return if (index < 0 || index >= arr.size) {
+            arr
+        } else (arr.indices)
+            .filter { i: Int -> i != index }
+            .map { i: Int -> arr[i] }
+            .toIntArray()
     }
 }
