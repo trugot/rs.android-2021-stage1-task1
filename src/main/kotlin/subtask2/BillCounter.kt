d54dc42c93c22ad4a26f07db426fb6da2fefefed
trugot <77696733+trugot@users.noreply.github.com>
2021-05-29 17:38:36 +0300

    fix: change BillCounter.kt

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,23 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val fairlySplit = "bon appetit"
+        var tempArray: IntArray = deleteItemInArr(bill, k)
+        var answer = tempArray.sum() /  2.0
+        if (answer.toInt() == b)
+            return fairlySplit
+        return(b - answer.toInt()).toString()
+    }
+
+    private fun deleteItemInArr(array: IntArray, index: Int): IntArray {
+        if (index < 0 || index >= array.size)
+            return array
+
+        var tempArray = IntArray(array.size - 1)
+        for (i in 0 until index)
+            tempArray[i] = array[i]
+        for (i in index until array.size - 1)
+            tempArray[i] = array[i + 1]
+        return tempArray
     }
 }
